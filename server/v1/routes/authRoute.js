f7f61542f29e0d177d78599c3be9f03d8f108279
devashish <it@policyplanner.com>
2026-01-06 18:34:42 +0530

    routes

---
+++ server/v1/routes/authRoute.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+const auth = require("../middlewares/auth");
+
+// User registration
+
+// user Login
+
+// edit Profile
+
+// add address
+
+// update address
+
+// delete address
+
+// fetch addresses
+
+// Get address By ID
+
+module.exports = router;
