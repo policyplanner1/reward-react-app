0c31043fdda7f86b849531b4167b22453d6da120
devashish <it@policyplanner.com>
2026-01-18 22:19:35 +0530

    css

--- client/src/components/feature/vendor/products/ProductManage.tsx
+++ client/src/components/feature/vendor/products/ProductManage.tsx
@@ -215,13 +215,13 @@ export default function ProductManage() {
                       onClick={() =>
                         toggleVisibility(v.variant_id, v.is_visible)
                       }
-                      className={`relative inline-flex h-6 w-11 items-center rounded-full transition ${
+                      className={`cursor-pointer relative inline-flex h-6 w-11 items-center rounded-full transition ${
                         v.is_visible ? "bg-green-500" : "bg-gray-300"
                       }`}
                       title={v.is_visible ? "Visible" : "Hidden"}
                     >
                       <span
-                        className={`inline-block h-4 w-4 transform rounded-full bg-white transition ${
+                        className={`cursor-pointer inline-block h-4 w-4 transform rounded-full bg-white transition ${
                           v.is_visible ? "translate-x-6" : "translate-x-1"
                         }`}
                       />
