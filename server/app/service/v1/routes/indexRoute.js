1fe75d25e5926c88ff9a271d2a915cbac8087942
devashish <it@policyplanner.com>
2026-01-22 16:01:39 +0530

    service document

--- server/app/service/v1/routes/indexRoute.js
+++ server/app/service/v1/routes/indexRoute.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const serviceCategoryRoute = require("./serviceCategoryRoute");
 const serviceRoute = require("./serviceRoute");
+const serviceDocumentRoute = require("./serviceDocumentRoute");
 
 router.use("/category", serviceCategoryRoute);
 router.use("/service", serviceRoute);
+router.use("/service-document",serviceDocumentRoute)
 
 module.exports = router;
