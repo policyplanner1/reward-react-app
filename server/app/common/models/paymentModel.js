c6c2e105b4c43a6f893ffaf804e9e24f6456e339
devashish <it@policyplanner.com>
2026-01-30 12:19:56 +0530

    payment update

--- server/app/common/models/paymentModel.js
+++ server/app/common/models/paymentModel.js
@@ -3,37 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 class PaymentModel {
-  async createTxn(data) {
-    const sql = `
-    INSERT INTO pg_transactions 
-    (order_id, merchant_txn_no, amount, pg_tran_ctx) 
-    VALUES (?, ?, ?, ?)
-  `;
-    await db.execute(sql, [
-      data.orderId,
-      data.merchantTxnNo,
-      data.amount,
-      data.tranCtx,
-    ]);
-  }
-
-  //   Status update
-  async updateStatus(merchantTxnNo, status, raw) {
-    const sql = `
-    UPDATE pg_transactions 
-    SET status = ?, raw_response = ?
-    WHERE merchant_txn_no = ?
-  `;
-    await db.execute(sql, [status, JSON.stringify(raw), merchantTxnNo]);
-  }
-
-  //   payment settlement
-  async markSettled(merchantTxnNo) {
-    await db.execute(
-      `UPDATE pg_transactions SET settlement_status = 1 WHERE merchant_txn_no = ?`,
-      [merchantTxnNo],
-    );
-  }
+ 
 }
 
 module.exports = new PaymentModel();
