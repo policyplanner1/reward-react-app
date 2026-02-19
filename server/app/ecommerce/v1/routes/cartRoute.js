ee1133a25806a0e60e59aa7626019142b65f006e
policyplanner1 <it@policyplanner.com>
2026-02-19 13:10:18 +0530

    cart updated

--- server/app/ecommerce/v1/routes/cartRoute.js
+++ server/app/ecommerce/v1/routes/cartRoute.js
@@ -5,26 +5,26 @@ const auth = require("../middlewares/auth");
 
 /* ======================================================cart============================================ */
 // get cart
-// router.get("/cart-items", auth, CartController.getCart);
-router.get("/cart-items", CartController.getCart);
+router.get("/cart-items", auth, CartController.getCart);
+// router.get("/cart-items", CartController.getCart);
 
 // add to cart
-// router.post("/cart-item", auth, CartController.addToCart);
-router.post("/cart-item", CartController.addToCart);
+router.post("/cart-item", auth, CartController.addToCart);
+// router.post("/cart-item", CartController.addToCart);
 
 // check quantity
 router.get("/cart-items/check-stock/:variantId", CartController.checkStock);
 
 // update cart
-// router.put("/cart-items/:cart_item_id", auth, CartController.updateCartItem);
-router.put("/cart-items/:cart_item_id", CartController.updateCartItem);
+router.put("/cart-items/:cart_item_id", auth, CartController.updateCartItem);
+// router.put("/cart-items/:cart_item_id", CartController.updateCartItem);
 
 // delete cart
-// router.delete("/cart-items/:cart_item_id", auth, CartController.deleteCartItem);
-router.delete("/cart-items/:cart_item_id", CartController.deleteCartItem);
+router.delete("/cart-items/:cart_item_id", auth, CartController.deleteCartItem);
+// router.delete("/cart-items/:cart_item_id", CartController.deleteCartItem);
 
 // remove all cart items
-// router.delete("/cart-items", auth, CartController.clearCart);
-router.delete("/cart-items", CartController.clearCart);
+router.delete("/cart-items", auth, CartController.clearCart);
+// router.delete("/cart-items", CartController.clearCart);
 
 module.exports = router;
