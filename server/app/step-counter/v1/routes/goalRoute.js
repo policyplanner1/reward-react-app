27c995116903a9326fade4e0b9d4ff7f705428dc
policyplanner1 <it@policyplanner.com>
2026-03-14 15:24:50 +0530

    route updated

--- server/app/step-counter/v1/routes/goalRoute.js
+++ server/app/step-counter/v1/routes/goalRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const goalController = require("../controllers/goalController");
+const auth = require("../../../ecommerce/v1/middlewares/auth");
 
 router.get("/goals", goalController.getGoals);
 
-router.post("/select-goal", goalController.selectGoal);
+router.post("/select-goal", auth, goalController.selectGoal);
 
-router.get("/user-goal/:user_id", goalController.getUserGoal);
+router.get("/user-goal/:user_id", auth, goalController.getUserGoal);
 
 module.exports = router;
