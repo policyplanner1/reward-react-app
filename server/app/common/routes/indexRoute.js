43c4fbacd7554af566ba63fd9d1a4c68943def03
policyplanner1 <it@policyplanner.com>
2026-03-18 01:57:08 +0530

    terms

--- server/app/common/routes/indexRoute.js
+++ server/app/common/routes/indexRoute.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const settingRoutes = require("./settingRoute");
 const supportRoutes = require("./supportRoute");
+const termRoutes = require("./termRoute");
 
 router.use("/settings", settingRoutes);
 router.use("/support", supportRoutes);
+router.use("/terms", termRoutes);
 
 module.exports = router;
