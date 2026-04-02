bca41d839ac89a6be713e17729dea49de360b391
policyplanner1 <it@policyplanner.com>
2026-04-02 10:57:59 +0530

    bbps phase-15

--- server/app/bbps/v1/routes/indexRoute.js
+++ server/app/bbps/v1/routes/indexRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const v1PaymentRoutes = require("./paymentRoute");
 const v1BillsRoutes = require("./billRoute");
 
-router.use("bills",v1BillsRoutes);
+router.use("/bills",v1BillsRoutes);
 router.use("/bill-pay", v1PaymentRoutes);
 
 module.exports = router;
