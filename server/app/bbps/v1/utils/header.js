66c833f2fc052caa31c00dc6692bb0e5496b5e37
policyplanner1 <it@policyplanner.com>
2026-04-02 11:17:26 +0530

    bbps phase-16

--- server/app/bbps/v1/utils/header.js
+++ server/app/bbps/v1/utils/header.js
@@ -10,7 +10,7 @@ const agent =
 const callHeaderAPI = async (endpoint, payload) => {
   try {
     const res = await axios.post(
-      `${process.env.BACKEND_URL}${endpoint}`,
+      `${process.env.HEADER_API_URL}${endpoint}`,
       payload,
       {
         headers: { "Content-Type": "application/json" },
