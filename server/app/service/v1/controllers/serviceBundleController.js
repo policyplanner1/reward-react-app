256f3c63c0a6dc7b38aa877b600e7e1b18b78a4b
policyplanner1 <it@policyplanner.com>
2026-04-15 10:46:39 +0530

    Service bundle phase-1

--- server/app/service/v1/controllers/serviceBundleController.js
+++ server/app/service/v1/controllers/serviceBundleController.js
@@ -1,7 +1,82 @@
 const db = require("../../../../config/database");
 
 class ServiceBundleController {
-  
+  // service bundle list
+  async getServiceBundles(req, res) {
+    try {
+      const [rows] = await db.execute(
+        `SELECT id, name, description, bundle_price, original_price, banner_image
+       FROM service_bundles
+       WHERE status = 1`,
+      );
+
+      res.json({
+        success: true,
+        data: rows,
+      });
+    } catch (error) {
+      console.error("Error fetching service bundles:", error);
+      res.status(500).json({ success: false, message: err.message });
+    }
+  }
+
+  //   Bundle by ID
+  async getServiceBundleDetail(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Bundle ID is required" });
+      }
+
+      // bundle
+      const [[bundle]] = await db.execute(
+        `SELECT * FROM service_bundles WHERE id = ?`,
+        [id],
+      );
+
+      if (!bundle) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Bundle not found" });
+      }
+
+      // items
+      const [items] = await db.execute(
+        `
+      SELECT 
+        s.name AS service_name,
+        sv.variant_name,
+        sv.image_url,
+        sv.title,
+        bi.price,
+        bi.is_required,
+        bi.service_id,
+        bi.variant_id
+
+      FROM service_bundle_items bi
+      JOIN services s ON s.id = bi.service_id
+      JOIN service_variants sv ON sv.id = bi.variant_id
+
+      WHERE bi.bundle_id = ?
+      ORDER BY bi.sort_order
+      `,
+        [id],
+      );
+
+      res.json({
+        success: true,
+        data: {
+          bundle,
+          items,
+        },
+      });
+    } catch (err) {
+      res.status(500).json({ success: false, message: err.message });
+    }
+  }
 }
 
 module.exports = new ServiceBundleController();
--- server/app/service/v1/routes/serviceBundleRoute.js
+++ server/app/service/v1/routes/serviceBundleRoute.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const ServiceBundleController = require("../controllers/serviceBundleController");
 const auth = require("../../../ecommerce/v1/middlewares/auth");
 
+// Get Bundles
+router.get("/", ServiceBundleController.getServiceBundles);
 
+// Get Bundle Detail
+router.get("/detail/:id", ServiceBundleController.getServiceBundleDetail);
 
 module.exports = router;
