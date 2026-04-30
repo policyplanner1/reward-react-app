ef2e4b3f2dea2a6e5eebbf9b85dc82883c313ad9
policyplanner1 <it@policyplanner.com>
2026-04-30 18:21:40 +0530

    auth updated

--- server/app/ecommerce/v1/controllers/authController.js
+++ server/app/ecommerce/v1/controllers/authController.js
@@ -408,140 +408,23 @@ class AuthController {
   /* ======================================================
      LOGIN
   ====================================================== */
-  // async loginUser(req, res) {
-  //   try {
-  //     const { email, password } = req.body;
-  //     const normalizedEmail = email.trim().toLowerCase();
-
-  //     const user = await AuthModel.findByEmail(normalizedEmail);
-  //     if (!user) return res.status(401).json({ success: false });
-
-  //     if (Number(user.status) !== 1)
-  //       return res
-  //         .status(403)
-  //         .json({ success: false, message: "Account inactive" });
-
-  //     if (!user.is_verified)
-  //       return res
-  //         .status(403)
-  //         .json({ success: false, message: "Email not verified" });
-
-  //     const match = await bcrypt.compare(password, user.password);
-  //     if (!match) return res.status(401).json({ success: false });
-  //     const accessToken = jwt.sign(
-  //       { user_id: user.user_id, token_version: user.token_version },
-  //       process.env.ACCESS_TOKEN_SECRET,
-  //       { expiresIn: ACCESS_EXPIRES },
-  //     );
-  //     const refreshToken = jwt.sign(
-  //       { user_id: user.user_id },
-  //       process.env.REFRESH_TOKEN_SECRET,
-  //       { expiresIn: `${REFRESH_EXPIRES_DAYS}d` },
-  //     );
-  //     const expiryDate = new Date(
-  //       Date.now() + REFRESH_EXPIRES_DAYS * 24 * 60 * 60 * 1000,
-  //     );
-  //     // check existing device
-  //     const deviceInfo = req.headers["user-agent"];
-  //     const ipAddress = req.ip;
-
-  //     // check if device already exists
-  //     const existingDevice = await AuthModel.checkExistingDevice(
-  //       user.user_id,
-  //       deviceInfo,
-  //     );
-
-  //     if (!existingDevice) {
-  //       await sendNewDeviceLoginEmail({
-  //         email: user.email,
-  //         name: user.name,
-  //         ip: ipAddress,
-  //         device: deviceInfo,
-  //       });
-  //     }
-
-  //     await AuthModel.storeRefreshToken(
-  //       user.user_id,
-  //       refreshToken,
-  //       expiryDate,
-  //       deviceInfo,
-  //       ipAddress,
-  //     );
-
-  //     await AuthModel.updateLoginMeta(user.user_id, req.ip);
-
-  //     const firstLoginBonus = await WalletModel.createWalletOnFirstLogin(
-  //       user.user_id,
-  //     );
-
-  //     if (firstLoginBonus) {
-  //       // Send Email
-  //       await rewardCreditMail({
-  //         email: user.email,
-  //         name: user.name,
-  //         coins: 3000,
-  //       });
-
-  //       // Send WhatsApp
-  //       // await sendWhatsAppWalletCredit({
-  //       //   phone: user.phone,
-  //       //   name: user.name,
-  //       //   coins: 3000,
-  //       // });
-  //     }
-
-  //     return res.json({
-  //       success: true,
-  //       accessToken,
-  //       refreshToken,
-  //       firstLoginReward: {
-  //         awarded: firstLoginBonus,
-  //         coins: firstLoginBonus ? 3000 : 0,
-  //       },
-  //     });
-  //   } catch (err) {
-  //     return res.status(500).json({ success: false });
-  //   }
-  // }
-
-  /* ======================================================
-   LOGIN  // sakshi edits
-====================================================== */
-
   async loginUser(req, res) {
     try {
-      const { email, password, device_id, device_name } = req.body;
-
-      if (!email || !password) {
-        return res.status(400).json({
-          success: false,
-          message: "Email and password are required",
-        });
-      }
-
+      const { email, password } = req.body;
       const normalizedEmail = email.trim().toLowerCase();
 
       const user = await AuthModel.findByEmail(normalizedEmail);
+      if (!user) return res.status(401).json({ success: false });
 
-      if (!user) {
-        return res.status(401).json({
-          success: false,
-        });
-      }
+      if (Number(user.status) !== 1)
+        return res
+          .status(403)
+          .json({ success: false, message: "Account inactive" });
 
-      if (Number(user.status) !== 1) {
-        return res.status(403).json({
-          success: false,
-          message: "Account inactive",
-        });
-      }
-
-      if (!user.is_verified) {
-        return res.status(403).json({
-          success: false,
-          message: "Email not verified",
-        });
-      }
+      if (!user.is_verified)
+        return res
+          .status(403)
+          .json({ success: false, message: "Email not verified" });
 
       const employee = await AuthModel.findEmployeeByEmail(normalizedEmail);
 
@@ -553,117 +436,55 @@ class AuthController {
       }
 
       const match = await bcrypt.compare(password, user.password);
-
-      if (!match) {
-        return res.status(401).json({
-          success: false,
-          message: "Invalid email or password",
-        });
-      }
-
-      if (!device_id) {
-        return res.status(400).json({
-          success: false,
-          message: "Device ID is required",
-        });
-      }
-
-      const currentDeviceId = device_id.toString();
-      const currentDeviceName = device_name
-        ? device_name.toString().toLowerCase()
-        : "unknown";
-
-      const ipAddress = req.ip;
-      const userAgent = req.headers["user-agent"] || "Unknown";
-
-  
-      if (!user.device_id) {
-        await AuthModel.updateCustomerDevice({
-          userId: user.user_id,
-          deviceId: currentDeviceId,
-          deviceName: currentDeviceName,
-          ipAddress,
-        });
-
-        user.device_id = currentDeviceId;
-        user.device_name = currentDeviceName;
-      }
-
-      if (user.device_id && user.device_id !== currentDeviceId) {
-        const approvalToken = crypto.randomBytes(32).toString("hex");
-        const expiresAt = new Date(Date.now() + 15 * 60 * 1000);
-
-        await AuthModel.deletePendingDeviceRequests(user.user_id);
-
-        await AuthModel.createDeviceChangeRequest({
-          userId: user.user_id,
-          email: user.email,
-          oldDeviceId: user.device_id,
-          newDeviceId: currentDeviceId,
-          newDeviceName: currentDeviceName,
-          token: approvalToken,
-          ipAddress,
-          userAgent,
-          expiresAt,
-        });
-
-        await sendDeviceChangeApprovalMail({
-          email: user.email,
-          name: user.name,
-          deviceName: currentDeviceName,
-          ipAddress,
-          userAgent,
-          token: approvalToken,
-        });
-
-        return res.status(403).json({
-          success: false,
-          device_verification_required: true,
-          message:
-            "New device detected. Approval email has been sent to your registered email.",
-        });
-      }
-
+      if (!match) return res.status(401).json({ success: false });
       const accessToken = jwt.sign(
-        {
-          user_id: user.user_id,
-          token_version: user.token_version,
-        },
+        { user_id: user.user_id, token_version: user.token_version },
         process.env.ACCESS_TOKEN_SECRET,
-        {
-          expiresIn: ACCESS_EXPIRES,
-        },
+        { expiresIn: ACCESS_EXPIRES },
       );
-
       const refreshToken = jwt.sign(
-        {
-          user_id: user.user_id,
-        },
+        { user_id: user.user_id },
         process.env.REFRESH_TOKEN_SECRET,
-        {
-          expiresIn: `${REFRESH_EXPIRES_DAYS}d`,
-        },
+        { expiresIn: `${REFRESH_EXPIRES_DAYS}d` },
       );
-
       const expiryDate = new Date(
         Date.now() + REFRESH_EXPIRES_DAYS * 24 * 60 * 60 * 1000,
       );
+      // check existing device
+      const deviceInfo = req.headers["user-agent"];
+      const ipAddress = req.ip;
+
+      // check if device already exists
+      const existingDevice = await AuthModel.checkExistingDevice(
+        user.user_id,
+        deviceInfo,
+      );
+
+      if (!existingDevice) {
+        await sendNewDeviceLoginEmail({
+          email: user.email,
+          name: user.name,
+          ip: ipAddress,
+          device: deviceInfo,
+        });
+      }
 
       await AuthModel.storeRefreshToken(
         user.user_id,
         refreshToken,
         expiryDate,
-        userAgent,
+        deviceInfo,
         ipAddress,
       );
 
-      await AuthModel.updateLoginMeta(user.user_id, ipAddress);
+      await AuthModel.updateLoginMeta(user.user_id, req.ip);
 
       const firstLoginBonus = await WalletModel.createWalletOnFirstLogin(
         user.user_id,
       );
 
       if (firstLoginBonus) {
+        // Send Email
         await rewardCreditMail({
           email: user.email,
           name: user.name,
@@ -684,25 +505,216 @@ class AuthController {
 
       return res.json({
         success: true,
-        message: "Login successful",
         accessToken,
         refreshToken,
-        device_id: currentDeviceId,
-        device_name: currentDeviceName,
         firstLoginReward: {
           awarded: firstLoginBonus,
           coins: firstLoginBonus ? 3000 : 0,
         },
       });
     } catch (err) {
-      console.error("LOGIN ERROR:", err);
-
-      return res.status(500).json({
-        success: false,
-        message: "Server error",
-      });
+      return res.status(500).json({ success: false });
     }
   }
+
+  /* ======================================================
+   LOGIN  // sakshi edits
+====================================================== */
+
+  // async loginUser(req, res) {
+  //   try {
+  //     const { email, password, device_id, device_name } = req.body;
+
+  //     if (!email || !password) {
+  //       return res.status(400).json({
+  //         success: false,
+  //         message: "Email and password are required",
+  //       });
+  //     }
+
+  //     const normalizedEmail = email.trim().toLowerCase();
+
+  //     const user = await AuthModel.findByEmail(normalizedEmail);
+
+  //     if (!user) {
+  //       return res.status(401).json({
+  //         success: false,
+  //       });
+  //     }
+
+  //     if (Number(user.status) !== 1) {
+  //       return res.status(403).json({
+  //         success: false,
+  //         message: "Account inactive",
+  //       });
+  //     }
+
+  //     if (!user.is_verified) {
+  //       return res.status(403).json({
+  //         success: false,
+  //         message: "Email not verified",
+  //       });
+  //     }
+
+  //     const employee = await AuthModel.findEmployeeByEmail(normalizedEmail);
+
+  //     if (!employee) {
+  //       return res.status(404).json({
+  //         success: false,
+  //         message: "Employee not found",
+  //       });
+  //     }
+
+  //     const match = await bcrypt.compare(password, user.password);
+
+  //     if (!match) {
+  //       return res.status(401).json({
+  //         success: false,
+  //         message: "Invalid email or password",
+  //       });
+  //     }
+
+  //     if (!device_id) {
+  //       return res.status(400).json({
+  //         success: false,
+  //         message: "Device ID is required",
+  //       });
+  //     }
+
+  //     const currentDeviceId = device_id.toString();
+  //     const currentDeviceName = device_name
+  //       ? device_name.toString().toLowerCase()
+  //       : "unknown";
+
+  //     const ipAddress = req.ip;
+  //     const userAgent = req.headers["user-agent"] || "Unknown";
+
+  //     if (!user.device_id) {
+  //       await AuthModel.updateCustomerDevice({
+  //         userId: user.user_id,
+  //         deviceId: currentDeviceId,
+  //         deviceName: currentDeviceName,
+  //         ipAddress,
+  //       });
+
+  //       user.device_id = currentDeviceId;
+  //       user.device_name = currentDeviceName;
+  //     }
+
+  //     if (user.device_id && user.device_id !== currentDeviceId) {
+  //       const approvalToken = crypto.randomBytes(32).toString("hex");
+  //       const expiresAt = new Date(Date.now() + 15 * 60 * 1000);
+
+  //       await AuthModel.deletePendingDeviceRequests(user.user_id);
+
+  //       await AuthModel.createDeviceChangeRequest({
+  //         userId: user.user_id,
+  //         email: user.email,
+  //         oldDeviceId: user.device_id,
+  //         newDeviceId: currentDeviceId,
+  //         newDeviceName: currentDeviceName,
+  //         token: approvalToken,
+  //         ipAddress,
+  //         userAgent,
+  //         expiresAt,
+  //       });
+
+  //       await sendDeviceChangeApprovalMail({
+  //         email: user.email,
+  //         name: user.name,
+  //         deviceName: currentDeviceName,
+  //         ipAddress,
+  //         userAgent,
+  //         token: approvalToken,
+  //       });
+
+  //       return res.status(403).json({
+  //         success: false,
+  //         device_verification_required: true,
+  //         message:
+  //           "New device detected. Approval email has been sent to your registered email.",
+  //       });
+  //     }
+
+  //     const accessToken = jwt.sign(
+  //       {
+  //         user_id: user.user_id,
+  //         token_version: user.token_version,
+  //       },
+  //       process.env.ACCESS_TOKEN_SECRET,
+  //       {
+  //         expiresIn: ACCESS_EXPIRES,
+  //       },
+  //     );
+
+  //     const refreshToken = jwt.sign(
+  //       {
+  //         user_id: user.user_id,
+  //       },
+  //       process.env.REFRESH_TOKEN_SECRET,
+  //       {
+  //         expiresIn: `${REFRESH_EXPIRES_DAYS}d`,
+  //       },
+  //     );
+
+  //     const expiryDate = new Date(
+  //       Date.now() + REFRESH_EXPIRES_DAYS * 24 * 60 * 60 * 1000,
+  //     );
+
+  //     await AuthModel.storeRefreshToken(
+  //       user.user_id,
+  //       refreshToken,
+  //       expiryDate,
+  //       userAgent,
+  //       ipAddress,
+  //     );
+
+  //     await AuthModel.updateLoginMeta(user.user_id, ipAddress);
+
+  //     const firstLoginBonus = await WalletModel.createWalletOnFirstLogin(
+  //       user.user_id,
+  //     );
+
+  //     if (firstLoginBonus) {
+  //       await rewardCreditMail({
+  //         email: user.email,
+  //         name: user.name,
+  //         coins: 3000,
+  //       });
+
+  //       // Send WhatsApp
+  //       await enqueueWhatsApp({
+  //         eventName: "wallet_credit_first_login",
+  //         ctx: {
+  //           phone: employee.phone,
+  //           company_id: employee.company_id,
+  //           customer_name: employee.name || "User",
+  //           coins: 3000,
+  //         },
+  //       });
+  //     }
+
+  //     return res.json({
+  //       success: true,
+  //       message: "Login successful",
+  //       accessToken,
+  //       refreshToken,
+  //       device_id: currentDeviceId,
+  //       device_name: currentDeviceName,
+  //       firstLoginReward: {
+  //         awarded: firstLoginBonus,
+  //         coins: firstLoginBonus ? 3000 : 0,
+  //       },
+  //     });
+  //   } catch (err) {
+  //     console.error("LOGIN ERROR:", err);
+
+  //     return res.status(500).json({
+  //       success: false,
+  //       message: "Server error",
+  //     });
+  //   }
+  // }
 
   /* ======================================================
    ALLOW DEVICE CHANGE
